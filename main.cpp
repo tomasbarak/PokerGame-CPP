068da1894eb02fa4ec34871519765753c2a3d3d5
tomasbarak <barakcinthia@gmalil.com>
2021-08-05 12:08:23 -0300

    Todas las functiones de mazo.cpp estan dentro de main.cpp ahora

--- main.cpp
+++ main.cpp
@@ -5,13 +5,40 @@
 
 #include "Carta.h"
 #include "Mensajes.h"
-#include "mazo.h"
 #include "Funciones_incompletas.h"
 
 using namespace std;
 
 int semilla_random = 1;
 
+std::vector<carta> crear_mazo(){
+    std::vector<carta> mazo;
+    for (int palo = 0; palo < 4; palo++) {
+        for (int valor = 1; valor < 14; valor++) {
+            carta c;
+            c.valor = valor;
+            c.palo = palo;
+            c.visible = valor_para_mostrar(c.valor) + palo_para_mostrar(c.palo);
+            mazo.push_back(c);
+        }
+    }
+    return mazo;
+}
+
+int indice_aleatorio(std::vector<carta> cartas){
+    srand (semilla_random);
+    semilla_random++;
+    return rand() % cartas.size();
+}
+
+std::vector<carta> repartir(std::vector<carta> &mazo, int cant_cartas){
+    std::vector<carta> mano;
+    for (int i = 0; i < cant_cartas; i++) {
+        mano.push_back(mazo[mazo.size()-1]);
+        mazo.pop_back();
+    }
+    return mano;
+}
 
 int main() {
     int cant_fichas_j1 = 15;
