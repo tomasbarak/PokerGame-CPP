64c5de54891ca53233d154045301125df4535283
ItsD4Rk <joaquinamaier@gmail.com>
2021-08-12 10:57:56 -0300

    Inicio del comparar_cartas.cpp

--- Comparar_cartas.cpp
+++ Comparar_cartas.cpp
@@ -1,6 +1,10 @@
 #include "Funciones_incompletas.h"
 
-bool comparar_cartas(std::vector<carta> mano_j1, std::vector<carta> mano_j2) {
-    //COMPLETAR.
+using namespace std;
+
+bool comparar_cartas(vector<carta> mano_j1, vector<carta> mano_j2) {
+    vector<vector<int>> jugadas =   { { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } };
+    vector<vector<int>> palos =     { { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } };
+
     return true;
 }
